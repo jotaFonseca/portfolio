a2b1dbf80a2b710e3cb95bb4912ba9e7e182dc8b
João Paulo Araújo <jpf.araujo99@hotmail.com>
2024-11-05 21:11:12 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,10 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        black: {
+          DEFAULT: '#000',
+          100: '#000319'
+        }
       },
     },
   },
